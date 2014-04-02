2c10867272e178f942cf5967d8d17b96c1aff5dc
sirian <sirianru@gmail.com>
2014-04-02 13:40:52 +0400

    Update UserNotFoundException.php

--- src/Admitad/UserBundle/Security/UserNotFoundException.php
+++ src/Admitad/UserBundle/Security/UserNotFoundException.php
@@ -3,25 +3,11 @@
 namespace Admitad\UserBundle\Security;
 
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class UserNotFoundException extends UsernameNotFoundException
 {
-    protected $token;
 
     protected $userData;
-
-    public function getToken()
-    {
-        return $this->token;
-    }
-
-    public function setToken(TokenInterface $token)
-    {
-        $this->token = $token;
-
-        return $this;
-    }
 
     public function getUserData()
     {
